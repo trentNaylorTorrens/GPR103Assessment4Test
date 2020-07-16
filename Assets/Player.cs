1c0375514d3ae395c11c72687016900e850b89ab
Trent <trent.naylor@laureate.edu.au>
2020-07-16 14:24:03 +1000

    My first project commit of the Assessment Skeleton.

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+/// <summary>
+/// This script must be used as the core Player script for managing the player character in the game.
+/// </summary>
+public class Player : MonoBehaviour
+{
+    public string playerName = ""; //The players name for the purpose of storing the high score
+   
+    public int playerTotalLives; //Players total possible lives.
+    public int playerLivesRemaining; //PLayers actual lives remaining.
+   
+    public bool playerIsAlive = true; //Is the player currently alive?
+    public bool playerCanMove = false; //Can the player currently move?
+
+    private GameManager myGameManager; //A reference to the GameManager in the scene.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+}
